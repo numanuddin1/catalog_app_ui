0b667823d4ffa2a6da9854c7e89ab1652ed9f2fa
Numan <nomanuddin2012@gmail.com>
2022-06-20 08:33:49 +0500

    dark and light theme

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -61,6 +61,7 @@ class _HomeState extends State<Home> {
               ),
               if(CatalogModel.items.isNotEmpty)
               Expanded(child: CatalogList())
+
               else
                 Expanded(child: Center(child: CircularProgressIndicator())),
             ],
